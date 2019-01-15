f5411e032a3691a7358a25f09d5524e14f7e70ce
importnetminecraft <devonrutledge03@gmail.com>
2019-01-15 06:36:16 -0700

    Fixed scrolling issues and issues with dragging elements via mouse.

--- src/com/importknowledge/lifemgr/panels/ListPanel.java
+++ src/com/importknowledge/lifemgr/panels/ListPanel.java
@@ -75,50 +75,58 @@ public class ListPanel extends ListPanelType implements MouseInputListener, Mous
 		repaint();
 	}
 	@Override
-	public void mouseEntered(MouseEvent e) {
-	}
+	public void mouseEntered(MouseEvent e) {}
 	@Override
 	public void mouseExited(MouseEvent e) {
+		selectdrag = false;
 	}
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		if(selectdrag){
-			List temp = ((OrderedList) list).getfocus().correct();
-			List temp2 = ((OrderedList) list).get(g2, (int) e.getX(), (int) e.getY()-(int) scroll).correct();
-			if(temp == temp2 && e.getX()-click.getX() > Settings.indent){
-				int i = temp.holder.items.indexOf(temp);
-				if(i > 0){
-					click.x = e.getX();
-					list.correct().remFrom(temp.holder, temp);
-					list.correct().addTo(temp.holder.items.get(i-1), temp);
+			ListInstance foc = ((OrderedList) list).getfocus();
+			List corrFoc = foc.correct();
+			ListInstance moused = ((OrderedList) list).get(g2, (int) e.getX(), (int) e.getY()-(int) scroll);
+			List corrMoused = moused == null?null:moused.correct();
+			if(!(corrFoc == null || corrFoc.holder == null || corrMoused == null || corrMoused.holder == null)){//do nothing
+				if(corrFoc.equals(corrMoused)){
+					if(e.getX()-click.getX() > Settings.indent){
+						int i = foc.holder.items.indexOf(foc);
+						if(i > 0){
+							click.x += Settings.indent;
+							((OrderedList) foc.holder).remove(foc);
+							foc.holder.items.get(i-1).add(corrFoc);
+						}
+					}else if(corrFoc.holder.holder != null && click.getX()-e.getX() > Settings.indent){
+						int i = corrFoc.holder.holder.items.indexOf(corrFoc.holder);
+						if(corrFoc.holder.holder != null && foc.holder.items.indexOf(foc) == foc.holder.items.size()-1){
+							click.x -= Settings.indent;
+							((OrderedList) foc.holder).remove(foc);
+							foc.holder.holder.add(i+1, corrFoc);
+						}
+					}
+				}else if(corrFoc.holder.equals(corrMoused.holder)){
+					int lineheight = corrFoc.name.lineheight(g2)+Settings.line;
+					int i = foc.holder.items.indexOf(foc);
+					if(Math.abs(click.getY()-e.getY())>2*lineheight){
+						selectdrag = false;
+					}else if(e.getY()-click.getY() > lineheight){
+						click.y += lineheight;
+						((OrderedList) foc.holder).remove(foc);
+						foc.holder.add(i+1, corrFoc);
+					}else if(click.getY()-e.getY() > lineheight){
+						click.y -= lineheight;
+						((OrderedList) foc.holder).remove(foc);
+						foc.holder.add(i-1, corrFoc);
+					}
 				}
-			}else if(temp == temp2 && click.getX()-e.getX() > Settings.indent){
-				if(temp.holder.holder != null){
-					int i = temp.holder.holder.items.indexOf(temp.holder);
-					click.x = e.getX();
-					list.correct().remFrom(temp.holder, temp);
-					list.correct().addTo(temp.holder.holder, i+1, temp);
-				}
-			}else if(temp.level() == temp2.level() && temp.holder == temp2.holder){
-				if(click2.getY() < e.getY()){
-					list.correct().set(temp2, temp);
-					list.correct().set(((OrderedList) list).getfocus(), temp2);
-				}else{
-					list.correct().set(((OrderedList) list).getfocus(), temp2);
-					list.correct().set(temp2, temp);
-				}
-			}else if(temp.level() > temp2.level() && temp.level() > 1){
-				if(click2.getY() > e.getY()){
-					List foc = ((OrderedList) list).getfocus().correct();
-					list.correct().remFrom(foc.holder, foc);
-					list.correct().addTo(foc.holder.holder, foc.holder.holder.items.indexOf(foc.holder), foc);
-				}else{
-					List foc = ((OrderedList) list).getfocus().correct();
-					list.correct().remFrom(foc.holder, foc);
-					list.correct().addTo(foc.holder.holder, foc.holder.holder.items.indexOf(foc.holder)+1, foc);
-				}
+				((OrderedList) foc.holder).setfocus((OrderedList) corrFoc.instance(list));
 			}
-		}else if(click2.getY()-e.getY()>0 || scroll<0) scroll -= click2.getY()-e.getY();
+		}else {
+			scroll -= click2.getY()-e.getY();
+			if(scroll > 0) scroll = 0;
+			var end = -Settings.line+getHeight()+(-list.countit())*(1.0*Settings.line+list.correct().name.lineheight(g2));
+			if(scroll < end) scroll = end;
+		}
 		click2 = e.getPoint();
 		list.update();
 		repaint();
@@ -131,7 +139,10 @@ public class ListPanel extends ListPanelType implements MouseInputListener, Mous
 
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent e) {
-		if((e.getWheelRotation()>0 && scroll>-Settings.line+getHeight()+(-list.countit())*(1.0*Settings.line+list.correct().name.lineheight(g2))) || (scroll<0 && e.getWheelRotation()<0))scroll -= Settings.scroll*e.getWheelRotation();
+		scroll -= Settings.scroll*e.getWheelRotation();
+		if(scroll > 0) scroll = 0;
+		var end = -Settings.line+getHeight()+(-list.countit())*(1.0*Settings.line+list.correct().name.lineheight(g2));
+		if(scroll < end) scroll = end;
 		repaint();
 	}
 	//endregion
